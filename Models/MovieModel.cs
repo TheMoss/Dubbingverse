714e1ab2ddc4cbc174637fe2ec75bc866fb79ea8
TheMoss <milena.k98@gmail.com>
2023-08-01 15:48:40 +0200

    Add a MovieModel for managing information about a movie (title, ID).

---
+++ Models/MovieModel.cs
@@ -0,0 +1,14 @@
+﻿namespace Dubbingverse.Models
+{
+    public class MovieModel
+    {
+        public string Title { get; set; }
+        public int PageID { get; set; }
+
+        public MovieModel(string title, int pageId)
+        {
+            Title = title;
+            PageID = pageId;
+        }
+    }
+}
