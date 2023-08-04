5e398ddbff197b1d43d3a1546cd13d10e31b500d
TheMoss <milena.k98@gmail.com>
2023-08-04 15:44:20 +0200

    Add ActorModel class that represents Actor entity.

---
+++ Models/ActorModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Dubbingverse.Models
+{
+	public class ActorModel
+	{
+		public string ActorName { get; set; }
+		public List<string> CharactersList { get; set; } = new List<string>();
+
+		public ActorModel(string actorName)
+		{
+			ActorName = actorName;
+		}
+	}
+}
